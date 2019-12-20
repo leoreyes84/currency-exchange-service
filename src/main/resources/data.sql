d824c00aec9c906fa19af5212702ec74959c50d8
Leonardo Reyes <leonardo.reyes@colfuturo.org>
2019-12-19 21:51:32 -0500

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values (1001,'USD','COL',3300,0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values (1002,'EUR','COL',4000,0);
+insert into exchange_value(id, currency_from, currency_to, conversion_multiple, port)
+values (1003,'AUD','COL',2000,0);
