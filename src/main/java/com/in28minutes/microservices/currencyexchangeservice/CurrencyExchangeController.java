284cda35c037c9cb4d11fd45991c0d79d1f63895
Leonardo Reyes <leonardo.reyes@colfuturo.org>
2019-12-26 21:14:04 -0500

    sleuth and rabbit dependencies

--- src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/in28minutes/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,7 +1,7 @@
 package com.in28minutes.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,9 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@Autowired
 	private Environment environment;
-	
+
 	@Autowired
 	private ExchangeValueRepository repository;
 
@@ -21,6 +23,7 @@ public class CurrencyExchangeController {
 	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
 		ExchangeValue exchangeValue = repository.findByFromAndTo(from, to);
 		exchangeValue.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+		logger.info("{}", exchangeValue);
 		return exchangeValue;
 	}
 
